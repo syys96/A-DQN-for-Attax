4e0ed01342f50f5c4041544a02fd23ad7d6051c9
syys96 <3359652182@qq.com>
2017-05-20 21:59:38 +0800

    Update runthis.py

--- runthis.py
+++ runthis.py
@@ -5,7 +5,7 @@ datasize = 1000
 blackNet = network.Network([49, 100, 200, 792])
 blacktraindata = generateTrainData.getTraindata(datasize,1)
 for i in range(1):
-    blackData = shelve.open('E:/blackData')
+    blackData = shelve.open('./blackData')
     try:
         blackNet.weights = blackData['weights']
         blackNet.biases = blackData['biaes']
